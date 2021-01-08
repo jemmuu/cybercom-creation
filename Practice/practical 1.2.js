a3a67d89a774eb7e06966b20f5e96b4de2c83a50
jemish kakadiya <jemishkakadiya.17.cs@iite.indusuni.ac.in>
2021-01-08 19:46:12 +0530

    html and js work 8th jan

---
+++ Practice/practical 1.2.js
@@ -0,0 +1,19 @@
+var BMI;
+
+//for mark
+
+var m=70;
+var h=1.2;
+
+mBmi=BMI = m / (h*h);
+
+// John
+
+m=78;
+h=1.5;
+
+jBmi=BMI = m / (h*h);
+
+console.log("Jhon's BMI = "+''+ jBmi );
+console.log("Mark's BMI = "+''+ mBmi);
+console.log("Is Marks's Highter Than John's?" + '  '+ (mBmi > jBmi));
