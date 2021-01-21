c5be68ce2f83f9eab1e50d070905ff68169dc446
jemish kakadiya <jemishkakadiya.17.cs@iite.indusuni.ac.in>
2021-01-21 11:27:52 +0530

    fst hour

---
+++ test(21-01-21)/js/Registeration.js
@@ -0,0 +1,35 @@
+
+function chkAdmin(argument) {
+	// body...
+
+	
+if (localStorage.adminData) {
+
+	alert('Admin already registered!');
+	document.getElementById('register_btn').disabled = true;
+}
+}
+
+// case of new admin profile
+
+function adminReg() {
+
+var adminData={
+
+		 name : document.getElementById('name').value,
+		 email : document.getElementById('email').value,
+		 password : document.getElementById('password').value,
+		 con_password : document.getElementById('con_password').value,
+		 city : document.getElementById('city').value,
+		 state : document.getElementById('state').value,
+
+
+}
+
+localStorage.setItem('adminData', JSON.stringify(adminData));
+
+
+
+
+//window.location.href='login.html';
+}
---
+++ test(21-01-21)/js/login.js
@@ -0,0 +1,30 @@
+
+
+
+//navigation function for register btn
+
+function navFun() {
+
+	window.location.href='Registeration.html';
+	
+}
+
+
+//login admin to dashboard
+
+
+function loginCheck() {
+	
+	let email = document.getElementById('email').value;
+	let password = document.getElementById('password').value;
+	let adminData = JSON.parse(localStorage.getItem('adminData'));
+	let adminPass=adminData[password];
+	let adminEmail=adminData[email];
+	console.log(adminPass);
+
+	if (email == adminEmail && password == adminPass) {
+
+
+		window.location.href= 'dashbord_admin.html';
+	}
+}
